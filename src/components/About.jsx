32be9be5d2ac3860eb6e3c10669bb0a188a96209
Mahyar <mahyar.erfanian1998@gmail.com>
2024-04-05 11:57:57 -0400

    Change about page content and minor style changes
    The content of the about page was changed to flow more nicely and minor
    style changes was done to make better use of the yellow font colors.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,47 +8,74 @@ const About = () => {
       </div>
       <div className="container container-small">
         <p>
-          Hello! My name is Mahyar Erfanian and my pronouns are he/they.
-          I&apos;ve taken a fascinating journey from engineering to the
-          captivating realm of coding! Originally from the land of pistachios,
-          sour cherries, and exquisite rugs, Iran, I have now settled in the
-          vibrant city of Montreal. I&apos;m fueled by a strong desire to solve
-          problems, which led me to switch gears and dive into web development
-          after completing my Masters in Engineering. Turns out, coding is where
-          I truly thrive!
+          Hey there! My name is Mahyar Erfanian (he/they) and I love making web
+          magic happen! My journey has taken me from the world of engineering to
+          the exciting realm of coding (seriously, problem-solving is my jam!).
+          When I decided to commit to being a coder for life, web development
+          felt like the perfect gateway and I&apos;ve been absolutely head over
+          heels for it ever since. Now I know for sure that coding is where I
+          truly thrive!
         </p>
         <p>
-          With expertise in both frontend and backend development, I specialize
-          in the MERN stack (MongoDB, Express.js, React.js, Node.js) and Ruby on
-          Rails. Whether it&apos;s building sleek user interfaces or engineering
-          robust server-side systems, I&apos;m always up for the task.
+          I completed my bachelor&apos;s in chemical engineering in 2020 and
+          master&apos;s in biomedical engineering in early 2023. I had all these
+          coding projects all over my university years but it was not long
+          before finishing my master&apos;s that I came into the realization
+          that coding has been my favorite part of engineering all along!
+          Learning and problem-solving is truly joyful to me and a career in
+          coding could promise me all that for every day of my life, so I jumped
+          right in with boundless motivation!
         </p>
         <p>
-          When I&apos;m not immersed in coding, you&apos;ll often find me
-          exploring my passion for cooking, a practice that helps me preserve
-          the cultural heritage I&apos;ve been raised with. I also find lots of
-          joy in expressing my creativity through{" "}
+          I began self-teaching web development in late 2022. It was
+          nerve-wracking at times not having a clear roadmap going the
+          self-taught route but that definitely did not affect my passion.
+          Despite challenges, I found reassurance in my decision every step of
+          the way. Completing the curriculum of{" "}
           <a
-            href="https://www.instagram.com/mahyar.erfn_art/"
             className="underlined"
+            href="https://www.theodinproject.com/"
+            target="__blank"
+          >
+            The Odin Project
+          </a>{" "}
+          by March 2024, I built this website as my first-ever real-world
+          project (after all, I&apos;m my own first customer!).
+        </p>
+        <p>
+          This website serves as my introduction to fellow developers and
+          potential collaborators! I am a full-stack web developer specialized
+          in both MERN stack (MongoDB, Express.js, React.js, Node.js) and Ruby
+          on Rails. This is also a place where I showcase my selected projects
+          and share insights through blog posts. These posts are mostly about
+          the challenges I face and how I tackle them, aiming to be of help to
+          both myself by bridging my knowledge gaps through writing and anyone
+          who is encountering similar issues.
+        </p>
+        <p>
+          To expand on who I am, I come from the land of pistachios and
+          exquisite rugs, Iran and have found a second home here in Montreal,
+          Canada since 2021. While finding joy in the world of tech and science
+          is and has been intentionally a big portion of my time, that is not my
+          only passion. You might often find me in the kitchen, exploring my
+          passion for cooking (a practice that helps me preserve my cherished
+          cultural heritage). Additionally, from time to time, I indulge in the
+          art of{" "}
+          <a
+            className="underlined"
+            href="https://www.instagram.com/mahyar.erfn_art/"
+            target="__blank"
           >
             portrait drawing
-          </a>
-          . Staying physically active is crucial in a career where long hours of
-          sedentary work is common, so I prioritize regular workouts to maintain
-          both my mental and physical well-being. Another passion close to my
-          heart is music and I particularly enjoy delving into the lyrical depth
-          of my favorite songs. Recently, I&apos;ve also begun writing a blog
-          focused on topics related to web development, which serves as a means
-          to continuously expand my knowledge and bridge any gaps in my
-          understanding of the field.
+          </a>{" "}
+          for leisure. Web development is my most recently-discovered passion,
+          guiding me to where I am today.
         </p>
         <p>
-          What excites me most about web development is the endless opportunity
-          for learning and growth and I embrace the role of a lifelong student.
-          I&apos;d love to create meaningful experiences and am eager to
-          collaborate on projects that challenge me. Let&apos;s work together to
-          turn your ideas into reality!
+          The opportunities to grow and learn in this field are endless and I am
+          committed to being a lifelong student. I would love to collaborate on
+          projects that challenge me and push me to new heights. Let&apos;s work
+          together and transform your ideas into reality!
         </p>
       </div>
     </>
